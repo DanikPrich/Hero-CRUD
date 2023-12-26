c2165286655066896e9a74d0c5f4cc6375418cb6
Daniel <Danil.danek@gmail.com>
2023-12-26 13:49:37 +0100

    create custom middleware and applying

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import reducer from '../reducers';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
@@ -26,8 +26,31 @@ const enhancer = (createStroe) => (...args) => {
   return store;
 }
 
-//если вторым аргументом мы помещаем какую то функцию то она усиляет наш стор  
-const store = createStore(combineReducers({heroes, filters}), enhancer);
+
+//создаем middleware который меняет dispatch
+//middleware возвращает стор (точнее его две сущности - dispatch и getState), если вдруг мы бы хотели достать значение стейта и что то с ним сделать
+//Согласен что тут сложная структура где функции возращаются три раза, но это реальный пример который применяется
+//Это создано для того чтобы в цепочке middleware-ов когда они запускаются друг за другом, они запускались последовательно и возвращали каждый раз диспетч
+//Таким образом в итоге образуется мощная функци
+//Если мы понимаем что вместо диспетч будет вызываться следующая функция у следующего middleware, мы заменяем dispatch на next
+const stringMiddleware = (store) => (next) => (action) => {
+  if (typeof action === 'string') {
+    return next({
+      type: action
+    })
+  }
+  //а если пришел обьект то мы ничего по сути не меняем и возвращаем диспетч с обьектом
+  return next(action)
+}
+
+//если вторым аргументом мы помещаем какую то функцию то она усиляет наш стор (используем компоуз для обьеденения)
+//Для того чтобы применить несколько middleware мы используем функцию applyMiddleware(...middleware)
+const store = createStore(
+  combineReducers({heroes, filters}),
+  // applyMiddleware(stringMiddleware), мы можем добавить вот таким образом миддлвееры или
+  compose(applyMiddleware(stringMiddleware),  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  // compose(enhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+);
 
 
 export default store;
