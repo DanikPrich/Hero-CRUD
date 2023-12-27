3f0977cb52eb763e87a504780e733c92d612b57f
Daniel <Danil.danek@gmail.com>
2023-12-27 12:12:04 +0100

    toolkit - configureStore function

--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,29 @@
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
-import reducer from '../reducers';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
-
-import {thunk} from 'redux-thunk'
+import { configureStore } from '@reduxjs/toolkit';
 
 //Создаем усилитель - enhancer
 //Помещаем создание стора в функцию которая вернет функцию стора с измененным диспетчем
 // ...args это аргументы которые мы поместим в createStore при первом вызове, 
-const enhancer = (createStroe) => (...args) => {
-  const store = createStore(...args);
+// const enhancer = (createStroe) => (...args) => {
+//   const store = createStore(...args);
 
-  //дальше мы сохраняем старый диспетч, он содержится в store.dispatch
-  const oldDispatch = store.dispatch;
-  //и меняем диспетч на другую функцию в которую будут приходить экшнс
-  store.dispatch = (action) => {
-    //если как экшн нам приходит строка то мы возвращаем старый диспетч с обьектом 
-    if (typeof action === 'string') {
-      return oldDispatch({
-        type: action
-      })
-    }
-    //а если пришел обьект то мы ничего по сути не меняем и возвращаем диспетч с обьектом
-    return oldDispatch(action)
-  }
-  //возвращаем стор с измененным диспетчем
-  return store;
-}
+//   //дальше мы сохраняем старый диспетч, он содержится в store.dispatch
+//   const oldDispatch = store.dispatch;
+//   //и меняем диспетч на другую функцию в которую будут приходить экшнс
+//   store.dispatch = (action) => {
+//     //если как экшн нам приходит строка то мы возвращаем старый диспетч с обьектом 
+//     if (typeof action === 'string') {
+//       return oldDispatch({
+//         type: action
+//       })
+//     }
+//     //а если пришел обьект то мы ничего по сути не меняем и возвращаем диспетч с обьектом
+//     return oldDispatch(action)
+//   }
+//   //возвращаем стор с измененным диспетчем
+//   return store;
+// }
 
 
 //создаем middleware который меняет dispatch
@@ -47,12 +44,17 @@ const stringMiddleware = (store) => (next) => (action) => {
 
 //если вторым аргументом мы помещаем какую то функцию то она усиляет наш стор (используем компоуз для обьеденения)
 //Для того чтобы применить несколько middleware мы используем функцию applyMiddleware(...middleware)
-const store = createStore(
-  combineReducers({heroes, filters}),
-  // applyMiddleware(stringMiddleware), мы можем добавить вот таким образом миддлвееры или
-  compose(applyMiddleware(thunk, stringMiddleware),  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-  // compose(enhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-);
+// const store = createStore(
+//   combineReducers({heroes, filters}),
+//   // applyMiddleware(stringMiddleware), мы можем добавить вот таким образом миддлвееры или
+//   compose(applyMiddleware(thunk, stringMiddleware),  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+//   // compose(enhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// );
 
-
+//новая команда которая создает стор и принимает обьект параметров
+const store  = configureStore({
+  reducer: {heroes, filters}, // Комбинируем два редьюсера 
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware), //[thunk, stringMiddleware], //подключаем дефолтные мидлвейры в массиве и добавляем новый наш мидлвейр
+  devTools: process.env.NODE_ENV !== 'production', //Вместо большой строки мы просто включаем девтулз, НО если мы оставим true то в проде у нас он тоже будет активен, поэтому проверяем проект на состояние (dev, prod)
+})
 export default store;
